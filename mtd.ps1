0b2fee8e8855cd60121b00c1d86c8f37cb35c49c
GanashreeRaj <ganashree.raj19@gmail.com>
2025-03-06 12:55:15 +0530

    Add files via upload

---
+++ mtd.ps1
@@ -0,0 +1,24 @@
+MKD COMPETATIVE PROGRAMMING
+
+DAY1 THURSDAY 6/03/2025
+
+important git commands
+
+$ git status
+// To know the curent status of our Repo
+
+$ git clone <URL>
+//To download a Repo
+
+git clone https://github.com/neelmyna/vvce_mar25
+// example to download the repo
+
+$ git pull origin main
+// Command to download the 
+$ git config --global user.name "neelmyna"
+$ git config --global user.email "nithin@gmail.com"
+
+git config --global user.name "neelmyna"
+git config --global user.email "nithin@gmail.com"
+
+git clone hteps://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@github.com/GanashreeRaj/MTD_C_PY
